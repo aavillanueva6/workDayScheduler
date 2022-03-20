41b43a6aa48c0ada1f4666063809675d45621a06
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-03-19 23:03:53 -0700

    adds checkTimes function to style for current time and adds basic save button click event handling

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,11 @@
 // Defines global variables used in multiple functions
+let currentTime;
+let storedText;
 
 // Defines DOM items that need handlers
+let mainContainer = $('#plannerDiv');
+let textBlocks = mainContainer.children().children('.agendaItems');
+// console.log(textBlocks);
 
 // Defines items that will utilize click handlers
 
@@ -14,6 +19,7 @@
 function init() {
   console.log('init called');
   getTimeDate();
+  checkTimes();
 }
 
 /**
@@ -29,14 +35,42 @@ function getTimeDate() {
       'Do'
     )}, ${now.format('YYYY')}`
   );
+  currentTime = parseInt(now.format('HH'));
+
   //TODO: add function calls to set class for textarea based on current time.
+}
+
+function checkTimes() {
+  $.each(textBlocks, function () {
+    let timeComparitor = parseInt($(this).attr('data-time'));
+    if (timeComparitor < currentTime) {
+      $(this).removeClass('future').removeClass('present').addClass('past');
+    } else if (timeComparitor === currentTime) {
+      $(this).removeClass('future').removeClass('past').addClass('present');
+      return;
+    } else {
+      $(this).removeClass('past').removeClass('present').addClass('future');
+    }
+  });
 }
 
 // Declares event listeners
 
 //TODO: currently this is for any click on the container... needs to be changed to be when current target is a saveBtn.
 //TODO: this currently just runs the init function.  actual functionality should run a function to save typed data, and run the getTimeDate function
-$('.container').on('click', init);
+$('.container').on('click', '.saveBtn', function (event) {
+  let btnClicked = $(event.target);
+  let parentOfBtn = btnClicked.parent('div');
+  let textAreaClicked = parentOfBtn.children('textarea');
+
+  console.log(parseInt(textAreaClicked.attr('data-time')));
+  let newItem = {
+    text: textAreaClicked.val(),
+    timeSlot: textAreaClicked.attr('data-time'),
+  };
+  console.log(newItem);
+  init();
+});
 
 // Calls initiliaztion function
 init();
